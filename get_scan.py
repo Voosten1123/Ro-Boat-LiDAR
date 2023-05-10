acd76687bfa71dcecaac2b24bd1da00fa4df9c11
Voosten1123 <127126248+Voosten1123@users.noreply.github.com>
2023-05-10 17:48:20 +0300

    Add files via upload

--- get_scan.py
+++ get_scan.py
@@ -1,18 +1,18 @@
 from rplidar import RPLidar
 from array import *
 import sys
+import time
 lidar = RPLidar('/dev/ttyUSB0')
 
-lidar.reset() #clears out buffer
-here = 'here'
 def get_scan():
     i = 0
     for scan in lidar.iter_scans():
         arr = scan            #list of unknown length (scan outputs random number [ 80-120 measurements]). [i][j] i = individual measurement, j: 0 quality, 1 angle, 2 distance
-        print(arr[0][1])       #confirmation for test
-        if arr[0][1] < 1 or arr[0][1] > 360:    #ensures that output arr starts at 000 deg.
+        #print(arr[0][1])       #confirmation for test
+        if i == 0:
+            i +=1
+        else:
             break
-    print(arr)
+    #print(arr)
+    lidar.stop()
     return arr
-
-get_scan()
